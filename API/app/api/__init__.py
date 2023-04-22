eba3e2e335eea7cc953680357e7398f7bd3e24da
Ruso kvesitadze <kvesitadzeruso@gmail.com>
2023-04-22 13:19:25 +0400

    add reset_password

--- API/app/api/__init__.py
+++ API/app/api/__init__.py
@@ -2,9 +2,13 @@ from flask_restful import Api
 from app.api.authentication import RegistrationApi, AuthorizationApi
 from app.api.address import AddressApi
 from app.api.card import CardsApi
+from app.api.reset_password import ForgotPasswordApi, ResetPasswordApi
 
 api = Api()
 api.add_resource(RegistrationApi, "/Registration")
 api.add_resource(AuthorizationApi, "/Authorization")
 api.add_resource(AddressApi, "/Address")
 api.add_resource(CardsApi, "/Cards")
+api.add_resource(ForgotPasswordApi, "/ForgotPassword")
+api.add_resource(ResetPasswordApi, "/ResetPassword")
+
