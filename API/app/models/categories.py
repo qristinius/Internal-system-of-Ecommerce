41371913879b2dd1175cdaa639ffda33e9535aa5
Kristine Dzneladze <qristina.qristina20010203@gmail.com>
2023-04-25 16:59:09 +0400

    added EAV method model of category , attr and val

--- API/app/models/categories.py
+++ API/app/models/categories.py
@@ -1,6 +1,16 @@
 from app.extensions import db
 from app.models.base import BaseModel
 
+
+class Value(BaseModel):
+    __tablename__ = "values"
+    id = db.Column(db.Integer, primary_key=True)
+
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+    attribute_id = db.Column(db.Integer, db.ForeignKey("attributes.id"))
+
+    value = db.Column(db.String)
+
 class Category(BaseModel):
     __tablename__ = "categories"
 
@@ -9,24 +19,18 @@ class Category(BaseModel):
 
     name = db.Column(db.String)
 
-    product = db.relationship("Product", backref = "category")
+    product = db.relationship("Product", backref="category")
 
-class SubCategory(BaseModel):
-    __tablename__ = "sub_category"
 
-    id = db.Column(db.Integer, primary_key=True)
-    parent_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
-
-    name = db.Column(db.String)
-    
-    characteristics = db.relationship("ProductCharacteristic", backref = "type")
-
-class ProductCharacteristic(BaseModel):
-    __tablename__ = "characteristic"
+class Attribute(BaseModel):
+    __tablename__ = "attributes"
     id = db.Column(db.Integer, primary_key=True)
 
-    name = db.Column(db.String)
-    subcategory_id = db.Column(db.Integer, db.ForeignKey("sub_category.id"))
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
 
+    label = db.Column(db.String)
+
+    product = db.relationship("Product", secondary = "values", backref = "attribute")
+    category = db.relationship("Category", backref ="category_attribute")
 
 
