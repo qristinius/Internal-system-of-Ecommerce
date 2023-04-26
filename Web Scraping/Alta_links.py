49650a154da724b0fd8e1b0e2da5054adc0b2f3a
Anri Tvalabeishvili <anri.tvalabeishvili1@gmail.com>
2023-04-27 01:51:09 +0400

    add web scraping materials

---
+++ Web Scraping/Alta_links.py
@@ -0,0 +1,116 @@
+links_for_pc_components = [
+    "PC Components",
+    "https://alta.ge/cpu.html",
+    "https://alta.ge/memory.html",
+    "https://alta.ge/motherboard.html",
+    "https://alta.ge/graphics-card.html",
+    "https://alta.ge/hdd-ssd.html",
+    "https://alta.ge/cooler.html",
+    "https://alta.ge/case.html",
+    "https://alta.ge/computers-and-office/pc-components/power-supplies.html"
+]
+
+links_for_monitors_projectors = [
+    "Monitors & Projectors",
+    "https://alta.ge/computers-and-office/monitors-and-projectors/monitors.html",
+    "https://alta.ge/computers-and-office/monitors-and-projectors/projectors.html",
+    "https://alta.ge/computers-and-office/monitors-and-projectors/projector-accessories.html"
+]
+
+links_for_tablets_accessories = [
+    "Tablets & Accessories",
+    "https://alta.ge/computers-and-office/tablets-and-accessories/tablets.html",
+    "https://alta.ge/computers-and-office/tablets-and-accessories/tablet-cases.html",
+    "https://alta.ge/computers-and-office/tablets-and-accessories/tablet-accessories.html"
+]
+
+links_for_computer_accessories = [
+    "Computer Accessories",
+    "https://alta.ge/computers-and-office/computer-accessories/keyboard.html",
+    "https://alta.ge/computers-and-office/computer-accessories/mouse.html",
+    "https://alta.ge/computers-and-office/computer-accessories/mouse-pad.html",
+    "https://alta.ge/computers-and-office/computer-accessories/speaker.html",
+    "https://alta.ge/computers-and-office/computer-accessories/headphone.html",
+    "https://alta.ge/computers-and-office/computer-accessories/microphone.html",
+    "https://alta.ge/computers-and-office/computer-accessories/webcam.html",
+    "https://alta.ge/computers-and-office/computer-accessories/external-hdd-ssd.html",
+    "https://alta.ge/computers-and-office/computer-accessories/usb-flash-drive.html",
+    "https://alta.ge/computers-and-office/computer-accessories/cables-and-adapters.html"
+]
+
+links_for_networking = [
+    "Networking",
+    "https://alta.ge/computers-and-office/networking/routers.html",
+    "https://alta.ge/computers-and-office/networking/wi-fi-adapters.html",
+    "https://alta.ge/computers-and-office/networking/network-hubs-and-switches.html",
+    "https://alta.ge/computers-and-office/networking/network-cables.html",
+]
+
+links_for_printers_scanners_supplies = [
+    "Printers Scanners Supplies",
+    "https://alta.ge/computers-and-office/printers-scanners-copiers-and-supplies/printers.html",
+    "https://alta.ge/computers-and-office/printers-scanners-copiers-and-supplies/cartridges.html",
+    "https://alta.ge/computers-and-office/printers-scanners-copiers-and-supplies/printer-and-copier-accessories.html",
+    "https://alta.ge/computers-and-office/printers-scanners-copiers-and-supplies/toners-and-drums.html"
+]
+
+links_for_notebooks_accessories = [
+    "Notebooks and Accessories",
+    "https://alta.ge/computers-and-office/pcs-notebooks-and-accessories/notebooks.html",
+    "https://alta.ge/computers-and-office/pcs-notebooks-and-accessories/notebook-bags.html",
+    "https://alta.ge/computers-and-office/pcs-notebooks-and-accessories/notebook-chargers.html"
+]
+
+links_for_phones_communications = [
+    "Phones & Communications",
+    "https://alta.ge/phones-and-communications/smartwatches-and-fitness-trackers.html",
+    "https://alta.ge/phones-and-communications/smart-watch-accessories.html",
+    "https://alta.ge/phones-and-communications/smartphones.html",
+    "https://alta.ge/phones-and-communications/smartphones-accessorise/screen-protectors.html",
+    "https://alta.ge/phones-and-communications/smartphones-accessorise/smartphone-cases.html",
+    "https://alta.ge/phones-and-communications/smartphones-accessorise/smartphone-chargers.html"
+]
+
+links_for_tV_photo_video_audio_game_consoles = [
+    "TV, Photo, Video, Audio & Game Consoles",
+    "https://alta.ge/tv-photo-video-audio-game-consoles/tvs.html",
+    "https://alta.ge/tv-photo-video-audio-game-consoles/tv-accessories.html",
+    "https://alta.ge/tv-photo-video-audio-game-consoles/audio-systems.html",
+    "https://alta.ge/tv-photo-video-audio-game-consoles/digital-cameras.html",
+    "https://alta.ge/tv-photo-video-audio-game-consoles/game-consoles-video-games.html"
+]
+
+links_for_home_appliances = [
+    "Home Appliances",
+    "https://alta.ge/home-appliance/home-appliances/refrigerators.html",
+    "https://alta.ge/home-appliance/home-appliances/washing-machines.html",
+    "https://alta.ge/home-appliance/home-appliances/drying-machines.html",
+    "https://alta.ge/home-appliance/home-appliances/dishwashers.html",
+    "https://alta.ge/home-appliance/home-appliances/gas-cookers.html",
+    "https://alta.ge/home-appliance/home-appliances/cooker-hood.html",
+    "https://alta.ge/home-appliance/home-appliances/built-in-hobs.html",
+    "https://alta.ge/home-appliance/home-appliances/built-in-electric-ovens.html"
+]
+
+links_for_kitchen_appliances = [
+    "Kitchen Appliances",
+    "https://alta.ge/home-appliance/kitchen-appliances/microwaves.html",
+    "https://alta.ge/home-appliance/kitchen-appliances/electric-ovens.html",
+    "https://alta.ge/home-appliance/kitchen-appliances/meat-choppers.html",
+    "https://alta.ge/home-appliance/kitchen-appliances/multicookers.html",
+    "https://alta.ge/home-appliance/kitchen-appliances/kitchen-machines.html",
+    "https://alta.ge/home-appliance/kitchen-appliances/bread-makers.html",
+    "https://alta.ge/home-appliance/kitchen-appliances/blenders-and-choppers.html",
+    "https://alta.ge/home-appliance/kitchen-appliances/electric-kettles.html",
+    "https://alta.ge/home-appliance/kitchen-appliances/juicers.html",
+    "https://alta.ge/home-appliance/kitchen-appliances/mixers.html",
+    "https://alta.ge/home-appliance/kitchen-appliances/toasters.html",
+    "https://alta.ge/home-appliance/kitchen-appliances/coffee-machines-and-boilers.html",
+]
+
+links_for_house = [
+    "House",
+    "https://alta.ge/home-appliance/house/vacuum-cleaners.html",
+    "https://alta.ge/home-appliance/house/vacuum-cleaner-accessories.html",
+    "https://alta.ge/home-appliance/house/irons.html",
+]
