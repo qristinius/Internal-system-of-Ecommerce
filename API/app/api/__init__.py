abc6ba456ab47978507b145551cf30a21396f1ed
Ruso kvesitadze <kvesitadzeruso@gmail.com>
2023-05-10 13:47:01 +0400

    add score api

--- API/app/api/__init__.py
+++ API/app/api/__init__.py
@@ -5,6 +5,7 @@ from app.api.card import CardsApi
 from app.api.reset_password import ForgotPasswordApi, ResetPasswordApi
 from app.api.comment import ProductCommentApi
 from app.api.mailconfirmation import ReceiveKeyApi, ConfirmEmailApi
+from app.api.score import ScoreApi
 
 api = Api()
 api.add_resource(RegistrationApi, "/Registration")
@@ -16,4 +17,5 @@ api.add_resource(ResetPasswordApi, "/ResetPassword")
 api.add_resource(ProductCommentApi,"/ProductComment")
 api.add_resource(ReceiveKeyApi, "/ReceiveKey")
 api.add_resource(ConfirmEmailApi,"/ConfirmEmail")
+api.add_resource(ScoreApi, "/Score")
 
