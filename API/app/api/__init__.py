c99ff4101d2030dd65cd8c8b27a79c97c9809d8f
Anri Tvalabeishvili <anri.tvalabeishvili1@gmail.com>
2023-05-25 21:01:40 +0400

    makeup changes

--- API/app/api/__init__.py
+++ API/app/api/__init__.py
@@ -6,6 +6,8 @@ from app.api.reset_password import ForgotPasswordApi, ResetPasswordApi
 from app.api.comment import ProductCommentApi
 from app.api.mailconfirmation import ReceiveKeyApi, ConfirmEmailApi
 from app.api.score import ScoreApi
+from app.api.product import ProductsApi
+
 
 api = Api()
 api.add_resource(RegistrationApi, "/Registration")
@@ -16,6 +18,8 @@ api.add_resource(ForgotPasswordApi, "/ForgotPassword")
 api.add_resource(ResetPasswordApi, "/ResetPassword")
 api.add_resource(ProductCommentApi, "/ProductComment")
 api.add_resource(ReceiveKeyApi, "/ReceiveKey")
-api.add_resource(ConfirmEmailApi,"/ConfirmEmail")
+api.add_resource(ConfirmEmailApi, "/ConfirmEmail")
 api.add_resource(ScoreApi, "/Score")
+api.add_resource(ProductsApi, "/Products")
+
 
